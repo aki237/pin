242f09255756527831baf4cdafba2de53fe61008
Akilan Elango <aki237@aki237.me>
2018-04-18 05:56:55 +0530

    separate the main login from the main function

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-
-	"./pinlib"
 )
 
 func main() {
@@ -30,41 +28,5 @@ func main() {
 		return
 	}
 
-	iface := NewTUN(ifaceName)
-	defer iface.Close()
-
-	var handler pinlib.Peer
-	var err error
-
-	if !*mode {
-		handler, err = pinlib.NewClient(*addr, 1, iface)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		SetupClient(handler.(*pinlib.Client), *addr, *ifaceName, *tunaddr, *gw)
-
-	} else {
-		handler, err = pinlib.NewServer(*addr, iface)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		err = SetupServer(handler.(*pinlib.Server), *ifaceName, *tunaddr)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-	}
-
-	err = handler.Start()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	iface.Close()
-
+	RunPin(*mode, *addr, *ifaceName, *tunaddr, *gw)
 }
---
+++ pin.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+
+	"./pinlib"
+)
+
+func RunPin(mode bool, addr, ifaceName, tunaddr, gw string) {
+
+	iface := NewTUN(&ifaceName)
+	defer iface.Close()
+
+	var handler pinlib.Peer
+	var err error
+
+	if !mode {
+		handler, err = pinlib.NewClient(addr, 1, iface)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		SetupClient(handler.(*pinlib.Client), addr, ifaceName, tunaddr, gw)
+
+	} else {
+		handler, err = pinlib.NewServer(addr, iface)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		err = SetupServer(handler.(*pinlib.Server), ifaceName, tunaddr)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		err = SetupServer(handler.(*pinlib.Server), ifaceName, tunaddr)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	err = handler.Start()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	iface.Close()
+
+}
