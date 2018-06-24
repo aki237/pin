5aee557f458a9605f9b0f71a986682739c518875
Akilan Elango <aki237@aki237.me>
2018-06-24 21:31:25 +0530

    removed commandline flags in favor of a config file.  * adapt the salsa20 changes in the core pinlib inn here

--- pin.go
+++ pin.go
@@ -1,28 +1,47 @@
 package main
 
 import (
+	"bufio"
+	"encoding/base64"
 	"fmt"
 	"net"
 	"os"
-	"os/signal"
+	"strings"
 	"syscall"
 
 	"./pinlib"
 )
 
-func RunPin(mode bool, addr, ifaceName, tunaddr, pub, key string) {
+const (
+	ERRSUCCESS    = 0
+	ERRRUNNING    = 3
+	ERRWARG       = 4
+	ERRNOTRUNNING = 5
+	ERRNOTIMPL    = 6
+)
+
+func RunPin(server bool, addr, ifaceName, tunaddr, secret string, px *Daemon, c chan os.Signal) {
 	iface := NewTUN(&ifaceName)
 	defer iface.Close()
 
 	var handler pinlib.Peer
 	var err error
 
-	if !mode {
-		handler = pinlib.NewClient(addr, iface, pub, key)
+	secretdec, err := base64.StdEncoding.DecodeString(secret)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-		SetupClient(handler.(*pinlib.Client), addr, ifaceName)
+	if len(secretdec) != 40 {
+		fmt.Println("Error : key length mismatch, need 40 got", len(secretdec))
+		return
+	}
 
-	} else {
+	var kcn [40]byte
+	copy(kcn[:], secretdec)
+
+	if server {
 		var ipNet *net.IPNet
 		var ip net.IP
 		ip, ipNet, err = net.ParseCIDR(tunaddr)
@@ -31,7 +50,7 @@ func RunPin(mode bool, addr, ifaceName, tunaddr, pub, key string) {
 			return
 		}
 		ipNet.IP = ip
-		handler, err = pinlib.NewServer(addr, iface, ipNet, pub, key)
+		handler, err = pinlib.NewServer(addr, iface, ipNet, kcn)
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -41,10 +60,17 @@ func RunPin(mode bool, addr, ifaceName, tunaddr, pub, key string) {
 			fmt.Println(err)
 			return
 		}
+	} else {
+		handler = pinlib.NewClient(addr, iface, kcn)
+
+		SetupClient(handler.(*pinlib.Client), addr, ifaceName)
 	}
 
-	c := make(chan os.Signal, 2)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGTSTP)
+	if px != nil {
+		px.peer = handler
+		px.isConfigured = true
+	}
+
 	go func() {
 		recdsig := <-c
 		switch recdsig {
@@ -63,5 +89,128 @@ func RunPin(mode bool, addr, ifaceName, tunaddr, pub, key string) {
 	}
 
 	iface.Close()
-	StopClient(addr)
+
+	if !server {
+		StopClient(addr)
+	}
+}
+
+type Daemon struct {
+	peer         pinlib.Peer
+	isConfigured bool
+	ln           net.Listener
+	running      bool
+	controller   chan os.Signal
+}
+
+func NewDaemon() *Daemon {
+	return &Daemon{controller: make(chan os.Signal, 2)}
+}
+
+func (d *Daemon) RunSocket(pidfile string) {
+	ln, err := net.Listen("unix", pidfile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	d.ln = ln
+
+	defer ln.Close()
+
+	d.running = true
+
+	for d.running {
+		conn, err := d.ln.Accept()
+		if d.running {
+
+			if err != nil {
+				fmt.Println("RunSocket : ", err)
+				continue
+			}
+		}
+		go d.Handle(conn)
+	}
+}
+
+func (d *Daemon) RunPin(server bool, addr, ifaceName, tunaddr, secret string) {
+	if server {
+		d.peer = new(pinlib.Server)
+	} else {
+		d.peer = new(pinlib.Client)
+	}
+
+	RunPin(server, addr, ifaceName, tunaddr, secret, d, d.controller)
+	d.isConfigured = false
+
+}
+
+func (d *Daemon) Handle(c net.Conn) {
+	rd := bufio.NewReader(c)
+
+	for {
+		line, err := rd.ReadString('\n')
+		if err != nil {
+			fmt.Println("RunSocket : Handle : ", err)
+			return
+		}
+
+		line = strings.TrimSpace(line)
+		keys := strings.Split(line, " ")
+		switch keys[0] {
+		case "connect":
+			if len(keys) != 4 {
+				fmt.Fprintf(c, "%d %s\n", ERRWARG, "wrong number of arguments")
+				break
+			}
+
+			if d.isConfigured {
+				fmt.Fprintf(c, "%d %s\n", ERRRUNNING, "already configured and running")
+				break
+			}
+
+			go d.RunPin(false, keys[1], keys[2], "", keys[3])
+
+			fmt.Fprintf(c, "%d %s\n", ERRSUCCESS, "success")
+		case "listen":
+			if len(keys) != 5 {
+				fmt.Fprintf(c, "%d %s\n", ERRWARG, "wrong number of arguments")
+				break
+			}
+			if d.isConfigured {
+				fmt.Fprintf(c, "%d %s\n", ERRRUNNING, "already configured and running")
+				break
+			}
+
+			go d.RunPin(true, keys[1], keys[2], keys[3], keys[4])
+
+			fmt.Fprintf(c, "%d %s\n", ERRSUCCESS, "success")
+
+		case "stop":
+			if !d.isConfigured {
+				fmt.Fprintf(c, "%d %s\n", ERRNOTRUNNING, "not configured")
+				break
+			}
+			d.controller <- os.Interrupt
+			fmt.Fprintf(c, "%d %s\n", ERRSUCCESS, "success")
+		case "stat":
+			if !d.isConfigured {
+				fmt.Fprintf(c, "%d %s\n", ERRNOTRUNNING, "not configured")
+				break
+			}
+
+			client, ok := d.peer.(*pinlib.Client)
+			if !ok {
+				fmt.Fprintf(c, "%d %s\n", ERRNOTIMPL, "not implemented")
+				break
+			}
+
+			txn := client.GetTxnStat()
+
+			fmt.Fprintf(c, "%d %d %d\n", ERRSUCCESS, txn.In, txn.Out)
+		case "exit":
+			fmt.Fprintf(c, "%d %s\n", ERRSUCCESS, "success")
+			d.running = false
+			d.ln.Close()
+		}
+	}
 }
